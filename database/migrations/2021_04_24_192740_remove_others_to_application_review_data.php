c7613ddda2563e505359d63385db764dc09e409c
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2021-04-24 19:30:58 +0800

    Removed others column to application review data table

---
+++ database/migrations/2021_04_24_192740_remove_others_to_application_review_data.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveOthersToApplicationReviewData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('application_review_data', function (Blueprint $table) {
+          $table->dropColumn(['others']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('application_review_data', function (Blueprint $table) {
+            //
+        });
+    }
+}
