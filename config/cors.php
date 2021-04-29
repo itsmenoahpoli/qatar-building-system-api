f11c28ce07c006784bd74bbcfcb47834c71a9adc
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2021-04-29 11:19:25 +0800

    Updated cors config for allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://dashboard.e-bpms.site', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => ['*'],
 
