a40c2ae1e17b8ebf30f1dc3c1beffdf59f90956b
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2021-05-07 04:34:27 +0800

    Invoices API - users invoice only return data if invoice data is present

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -182,7 +182,7 @@ class UsersController extends Controller
 
         if(count($invoices) > 0) {
           foreach($invoices as $invoice) {
-            $application = ApplicationRecord::where('user_id', $invoice->application_record_id)->get();
+            $application = ApplicationRecord::where('user_id', $invoice->application_record_id)->get()[0];
             array_push($user_invoices, $application->user_id);
             // if($invoice && $user_id == $invoice['application_record']['user_id']) {
             //   array_push($user_invoices, $invoice);
